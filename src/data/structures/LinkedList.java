5208c8f23aa2073fc147ce04d79f99a9bf1194d9
Jakub Slawinski <jakubslawinsky@gmail.com>
2022-06-07 19:48:05 +0200

    start creating LinkedList structure

---
+++ src/data/structures/LinkedList.java
@@ -0,0 +1,43 @@
+package data.structures;
+
+import org.w3c.dom.Node;
+
+public class LinkedList {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static LinkedList insert(LinkedList list, int data) {
+        Node newNode = new Node(data);
+        newNode.next = null;
+
+        if (list.head == null) {
+            list.head = newNode;
+        } else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+
+        return list;
+    }
+
+    public static void print(LinkedList list) {
+        Node currNode = list.head;
+        System.out.println("LinkedList: ");
+        while(currNode != null) {
+            System.out.print(currNode + " ");
+            currNode = currNode.next;
+        }
+    }
+}
---
+++ src/data/structures/Main.java
@@ -0,0 +1,11 @@
+package data.structures;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insert(linkedList, 5);
+        linkedList.insert(linkedList, 4);
+        linkedList.insert(linkedList, 3);
+        linkedList.insert(linkedList, 2);
+    }
+}
